41186f9d3039f998d2e78d582dbbdb355badecbc
vidhi chandrakar <vidhichandrakar.1701@gmail.com>
2023-12-28 16:03:02 +0530

    login page right

--- src/Components/LoginComponent/RightComponent.js
+++ src/Components/LoginComponent/RightComponent.js
@@ -2,14 +2,13 @@ import React, { Fragment } from "react";
 import { Box, Typography } from "@mui/material";
 import Logo from "../../Images/Logo.png"
 import FirstLoginOption from "./RightLoginSteps/FirstLoginOption";
+import RightTextBox from "./RightLoginSteps/RightTextBox";
  const RightComponent =({})=>{
   return(
     <Box className="rightBox">
       <Box className="rightTopBox">
        <img src={Logo} className="rightLogo"/>
-       <Typography className="rightTopHead">Welcome To</Typography>
-       <Typography className="rightTopHead2">360 Critcial Care</Typography>
-       <Typography className="rightTopHead3">World Class Preparation Just on tap away</Typography>
+        <RightTextBox/>
       </Box>
       <FirstLoginOption/>
     </Box>
