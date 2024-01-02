7e7f96eeb4026c32b1487e248b8557bded3b4d52
Shrishti2698 <shrishti260498@gmail.com>
2024-01-02 07:29:57 +0530

    dashboard course

--- src/Components/DashboardComponents/AllCOmponents/Home.js
+++ src/Components/DashboardComponents/AllCOmponents/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AllCourses from "../AllCoursesService/AllCourses";
 import YourPurchasedCourses from "../AllCoursesService/YourPurchasedCourses";
+import Explore from "../AllCoursesService/Explore";
 
 function Home() {
   return (
     <main className="main-container">
     {/* <AllCourses /> */}
-    <YourPurchasedCourses />
+    {/* <YourPurchasedCourses /> */}
+    <Explore/>
     </main>
   );
 }
